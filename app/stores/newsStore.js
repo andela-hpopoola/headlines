66b9fe83421280059ba1242b65bc9d7c38ecb782
Haruna Popoola <haruna.popoola@andela.com>
2017-04-24 17:39:03 +0100

    create the news store file

---
+++ app/stores/newsStore.js
@@ -0,0 +1,82 @@
+
+import { EventEmitter } from 'events';
+import Dispatcher from '../dispatcher/appDispatcher';
+import ActionTypes from '../constants/actionTypes';
+import NewsApi from '../api/NewsApi';
+
+const CHANGE_EVENT = 'change';
+let sources = [];
+let filteredSources = [];
+let news = [];
+let sortBy = [];
+
+const NewsStore = Object.assign(EventEmitter.prototype, {
+
+  emitChange() {
+    this.emit(CHANGE_EVENT);
+  },
+
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  },
+
+  getNews() {
+    return news;
+  },
+
+  getAllSources() {
+    // console.log(sources,'In get all sources');
+    // sources = NewsApi.getAllSources();
+    return sources;
+  },
+
+  getSources() {
+    return filteredSources;
+  },
+
+  getSourceObj(sourceId) {
+    const returnedSources = sources.filter(
+      source => source.id === sourceId
+    );
+    return {
+      name: returnedSources[0].name,
+      sortBy: returnedSources[0].sortBysAvailable,
+      description: returnedSources[0].description,
+      category: returnedSources[0].category,
+    };
+  },
+
+  searchSources(queryText) {
+    return sources.filter(
+      source => source.name.toLowerCase().indexOf(queryText) !== -1
+    );
+  },
+
+
+  dispatcherIndex: Dispatcher.register((action) => {
+    switch (action.actionType) {
+      case ActionTypes.ALL_SOURCES:
+        sources = action.sources;
+        break;
+
+      case ActionTypes.ALL_NEWS:
+        news = action.articles;
+        console.log(news, 'these are the news');
+        break;
+
+      case ActionTypes.SEARCH_SOURCES:
+        filteredSources = NewsStore.searchSources(action.queryString);
+        break;
+
+      default:
+      // no op
+    }
+    NewsStore.emitChange();
+  })
+});
+
+export default NewsStore;

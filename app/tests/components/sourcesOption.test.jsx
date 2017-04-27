501ce99f0840fbb185938f713118e2df9933b74d
Haruna Popoola <haruna.popoola@andela.com>
2017-04-27 15:29:18 +0100

    fix: add sourcesoption file load sources

---
+++ app/tests/components/sourcesOption.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import expect from 'expect';
+import $ from 'jQuery';
+import TestUtils from 'react-dom/test-utils';
+import SourcesOption from '../../components/SourcesOption.jsx';
+
+describe('SourcesOption', () => {
+  it('should exist', () => {
+    expect(SourcesOption).toExist();
+  });
+});

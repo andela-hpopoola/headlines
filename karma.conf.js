1fb482bc315659530a1670b79abba7b6229bd3e4
Haruna Popoola <haruna.popoola@andela.com>
2017-04-27 16:26:02 +0100

    update test configurations

--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,37 @@
 const webpackConfig = require('./webpack.config.js');
 
-module.exports = (config) => {
-  config.set({
-    browsers: ['Chrome'],
-    singleRun: true,
-    frameworks: ['mocha'],
-    files: ['app/tests/**/*.test.jsx'],
-    preprocessors: {
-      'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
-    },
-    reporters: ['mocha'],
-    client: {
-      mocha: {
-        timeout: '5000'
-      }
-    },
-    webpack: webpackConfig,
-    webpackServer: {
-      noInfo: true
+let configuration = {};
+
+configuration = {
+  browsers: ['Chrome'],
+  singleRun: true,
+  frameworks: ['mocha'],
+  files: ['app/tests/**/*.test.jsx'],
+  preprocessors: {
+    'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
+  },
+  reporters: ['mocha'],
+  client: {
+    mocha: {
+      timeout: '5000'
     }
-  });
+  },
+  customLaunchers: {
+    Chrome_travis_ci: {
+      base: 'Chrome',
+      flags: ['--no-sandbox']
+    }
+  },
+  webpack: webpackConfig,
+  webpackServer: {
+    noInfo: true
+  }
+};
+
+if (process.env.TRAVIS) {
+  configuration.browsers = ['Chrome_travis_ci'];
+}
+
+module.exports = (config) => {
+  config.set(configuration);
 };

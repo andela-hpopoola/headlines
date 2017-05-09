2b8c9ab7a07a8c355ad91c8c4c8341fc274933ca
Haruna Popoola <haruna.popoola@andela.com>
2017-05-09 08:40:31 +0100

    add link to article title and image

--- app/components/NewsSingle.jsx
+++ app/components/NewsSingle.jsx
@@ -14,34 +14,41 @@ const NewsSingle = (props) => {
   return (
       <div className="col-sm-6">
         <div className="thumbnail article">
+          {/* Article Image */}
+          <a href={ article.url } target="_blank">
             <div className="article__image">
               <img src={ article.urlToImage } alt={ article.title } className="img-responsive" />
             </div>
-            <div className="caption article__body">
-              <p className="article__intro">{publishedDate}</p>
-              <h3 className="article__title">{ article.title }</h3>
+          </a>
+          <div className="caption article__body">
+            <p className="article__intro">{publishedDate}</p>
+            <h3>
+              <a className="article__title" href={ article.url } target="_blank">
+                { article.title }
+              </a>
+            </h3>
 
-              <div>
-                <p className="article__lower">
-                  <strong className="article__author">{ article.author }</strong>
-                </p>
-              </div>
-
-              <hr />
-
-              <div className="row">
-                <div className="col-xs-8">
-                  <Share url={article.url} title={article.title} />
-                </div>
-                <div className="col-xs-4">
-                  <a
-                  href={ article.url }
-                  className="btn btn-primary btn-read-more"
-                  target="_blank"> Read More </a>
-                </div>
-              </div>
-
+            <div>
+              <p className="article__lower">
+                <strong className="article__author">{ article.author }</strong>
+              </p>
             </div>
+
+            <hr />
+
+            <div className="row">
+              <div className="col-xs-8">
+                <Share url={article.url} title={article.title} />
+              </div>
+              <div className="col-xs-4">
+                <a
+                href={ article.url }
+                className="btn btn-primary btn-read-more"
+                target="_blank"> Read More </a>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
   );

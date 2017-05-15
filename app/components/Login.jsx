96c4158b9fd5aa517c687645df4b1a253c58c287
Haruna Popoola <haruna.popoola@andela.com>
2017-05-15 09:50:49 +0100

    merge all instances of items used in localStorage

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -12,18 +12,20 @@ const getResponseFromGoogle = (response) => {
   const profile = response.profileObj;
   const history = createHistory({ forceRefresh: true });
   if (profile) {
-    // Save Output from Google
-    global.window.localStorage.setItem('profile', JSON.stringify(profile));
-
-    // Set Expiry Datevar today = new Date();
+    // Set Expiry Date and add to profile
     const today = new Date();
-    const expiryDate = new Date(today.getTime() + (24 * 60 * 60 * 1000));
-    global.window.localStorage.setItem('expiryDate', JSON.stringify(expiryDate));
+    profile.expiryDate = new Date(today.getTime() + (24 * 60 * 60 * 1000));
+    // Save Output from Google with expiryDate
+    global.window.localStorage.setItem('profile', JSON.stringify(profile));
+    // Redirect to news page
     history.push('/#/news');
     global.window.location.reload();
   }
 };
 
+// Google Client ID
+let clientID = '85385577141-0nb9rf764mh1eetjmd8p5jjmthr8e8hq';
+clientID += '.apps.googleusercontent.com';
 /**
  * Class displaying Login Page
  * @extends React.Component
@@ -45,8 +47,8 @@ class Login extends Component {
                 <img src="./images/logo_inverse.png" alt="Headlines Logo"/>
                 <h3>Get Live Headlines from over 70 Sources</h3>
                 <GoogleLogin
-                  clientId="85385577141-0nb9rf764mh1eetjmd8p5jjmthr8e8hq.apps.googleusercontent.com"
-                  buttonText="Login with Google"
+                  clientId= {clientID}
+                  buttonText='Login with Google'
                   onSuccess={getResponseFromGoogle}
                   onFailure={getResponseFromGoogle}
                 />
